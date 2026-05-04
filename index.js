72dc7e765b52e7494bdc06133c18bc632339743b
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2026-05-04 20:04:05 -0300

    adjst

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const {Pool} = pg
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: false
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false
 });
 
 const app = express();
