f83d7e7c0929470fe9ee61b76abd60a2b6f6c72b
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2026-05-09 20:09:35 -0300

    use_list_cost

--- index.js
+++ index.js
@@ -353,8 +353,7 @@ async function getItemShippingCost(accessToken, userId, item) {
     // retorna o menor custo disponível entre as opções
     console.log('SHIPPING RESPONSE:', JSON.stringify(data, null, 2)); // 👈 adiciona isso
 
-    const costs = data.coverage?.all_country?.list || [];
-    return costs.length ? costs[0].price : null;
+    return data.coverage?.all_country?.list_cost ?? null;
   } catch {
     return null;  // se falhar, não quebra o fluxo
   }
